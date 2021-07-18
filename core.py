53a6adb1d4ed437604895bee0f7e85d1fc4d2e49
ZazekSJ <73258347+ZazekSJ@users.noreply.github.com>
2021-07-18 21:34:28 +0200

    Add files via upload

---
+++ core.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+
+# Variables - customize your bot here
+prefix = '+'
+filtered_words = {'YourWords', 'GoHere'}
+welcome_message = f'{member} joined the server!'  # Change your welcome message here
+
+# Non customizable variables
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix=prefix, intents=intents)
+
+
+@bot.event
+async def on_ready():                                   # Shows if your bot is ready
+    print('{0.user} is ready! ✔️'.format(bot))
+
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(847949897445408821)
+    await channel.send(welcome_message)
+
+
+@bot.event
+async def on_message(message, limit=1):
+    if message.author.id == bot.user.id:
+        return
+
+    for word in filtered_words:
+        if word in message.content:
+            await message.delete()
+    await bot.process_commands(message)
+
+
+bot.run('Your token goes here')
